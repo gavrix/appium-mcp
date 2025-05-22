f882c77c6aa5b8a9add308c4344b96cdf2da923a
Sergey Gavrilyuk <sergey.gavrilyuk@gmail.com>
2025-05-22 00:17:29 -0600

    Configure package to be executable with npx

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod"; // We'll likely use Zod for schema validation later
